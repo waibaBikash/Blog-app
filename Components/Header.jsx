00b0ecd7bf8857644ac181d2b92682f94d04a56e
waibaBikash <kavrelibikash@gmail.com>
2024-06-26 15:16:55 +1000

    create a header section

---
+++ Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='py-5 px-5 md:px-12 lg:px-28'>
+      <div className='flex justify-between items-center'>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Header;
