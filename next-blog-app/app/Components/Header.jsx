2e872e6be7110e1e20f84d6b19d707464fe13757
waibaBikash <kavrelibikash@gmail.com>
2024-06-28 16:50:39 +1000

    commit change

--- next-blog-app/app/Components/Header.jsx
+++ next-blog-app/app/Components/Header.jsx
@@ -1,12 +1,14 @@
+import { assets } from '@/Assets/assets';
+import React from 'react';
+import Image from 'next/image';
 import React from 'react';
 
 const Header = () => {
   return (
     <div className='py-5 px-5 md:px-12 lg:px-28'>
       <div className='flex justify-between items-center'>
-
+      <Image src={assets.logo} />
       </div>
-
     </div>
   )
 }
--- next-blog-app/app/page.js
+++ next-blog-app/app/page.js
@@ -1,9 +1,8 @@
-import Header from "@/Components/Header";
-
+import Header from '@/app/Components/Header'
 export default function Home() {
   return (
     <>
-    <Header />
+     <Header/>
     </>
   );
 }
